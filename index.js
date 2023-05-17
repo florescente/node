564328c9fe98a2ef88bde0ef84f3af861baa93cd
florescente <campoflorescente@gmail.com>
2023-05-17 16:20:19 -0300

    mvc

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
+require('dotenv').config();
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
 
 const app = express();
+
+const uri = `mongodb+srv://nodetutorial:${process.env.DB_PASS}@nodetutorial.jjisjnj.mongodb.net/nodetutorial?retryWrites=true&w=majority`;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
 
 // register view engine
 app.set('view engine', 'ejs');
 
-app.listen(3000);
-
 // middleware static files
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan("tiny"));
 
+app.use((req, res, next) => {
+    res.locals.path = req.path;
+    next();
+  });
+
 app.get('/', function (req, res) {
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    ];
-    res.render('index', { title: 'Home', blogs });
+    res.redirect('/blogs');
 });
 
 app.get('/about', function (req, res) {
     res.render('about', { title: 'About' });
 });
 
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Blogs' });
-})
+// blog routes
+app.use('/blogs', blogRoutes);
 
 // redirect
 app.get('/about-us', (req, res) => {
